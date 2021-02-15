1454a1c070887f4da56557d1cc6cbc4e1a77a286
Arsen Arakelyan <aarakelyan98@gmail.com>
2021-02-15 12:39:37 +0200

    add converting file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Awesome Converter build with NodeJS, Express, TypeScript, TypeORM, RabbitMQ, PostgreSQL
+
+Steps to run this project:
+
+1. Run `npm i` command
+2. Setup database settings inside `ormconfig.json` file
+3. Run `npm start` command or `npm run dev`
---
+++ converter.service.ts
@@ -0,0 +1,31 @@
+class Converter {
+  // https://sharp.pixelplumbing.com/api-output
+  // https://www.npmjs.com/package/sharp нужен класс конвертер,
+  // у которого будут методы для работы с разными форматами
+  // https://medium.com/better-programming/sharp-high-performance-node-js-image-processing-library-3f04df66c722
+  // https://www.youtube.com/watch?v=1oVme5nIEpY
+
+  // if (req.file === '.jpg' || req.file === '.jpeg') {
+  // await sharp(photo).png({ quality: 100 });
+  // } else if (req.file === '.png') {
+  //   await sharp().jpeg({ quality: 100 });
+  // } else {
+  //   return req.file
+  // }
+
+  async convertJpgToPng(req: express.Request, res: express.Response) {
+    const data = sharp().png();
+  }
+
+  async convertPngToJpg(req: express.Request, res: express.Response) {
+    const data = sharp().jpeg();
+  }
+
+  async convertSvgToPng(req: express.Request, res: express.Response) {
+    const data = sharp().png();
+  }
+
+  async convertPngToSvg(req: express.Request, res: express.Response) {
+    const data = sharp().toBuffer(); // как в svg поменять???
+  }
+}
