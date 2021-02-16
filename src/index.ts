cc252fcef5729d1b01bcbdba1daf2ebbaab98925
Arsen Arakelian <arsen.arakelian@chisw.com>
2021-02-16 19:55:18 +0200

    trying to fix conn with services

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,7 @@ import * as dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT || 4000;
-// const DB_PORT = 3000 add this port in ormconfig when working from office;
-// const DB_PORT = 5432 add this port in ormconfig when working from home;
+const PORT = process.env.APP_PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- src/services/rabbitmq.service.ts
+++ src/services/rabbitmq.service.ts
@@ -1,45 +1,51 @@
-import * as amqp from 'amqplib';
+import * as amqp from 'amqplib/callback_api';
 
+// Docker:
 // add docker-compose file
 // https://docs.docker.com/compose/
 // https://docs.docker.com/get-started/overview/
 
-// https://www.youtube.com/watch?v=XrkNwwVLyOY&t=236s
-// https://habr.com/ru/post/447074/
+// RabbitMQ:
+// https://www.cloudamqp.com/blog/2015-05-18-part1-rabbitmq-for-beginners-what-is-rabbitmq.html
+// https://www.cloudamqp.com/blog/2015-05-19-part2-2-rabbitmq-for-beginners_example-and-sample-code-node-js.html
 // https://www.rabbitmq.com/getstarted.html
 // https://stackify.com/message-queues-12-reasons/
-// https://refactoring.guru/ru/design-patterns/singleton
-// https://metanit.com/sharp/patterns/2.3.php
-// https://medium.com/better-programming/implementing-rabbitmq-with-node-js-93e15a44a9cc
+// https://blog.logrocket.com/understanding-message-queuing-systems-using-rabbitmq/
 // https://medium.com/hepsiburadatech/migrating-rabbitmq-in-a-high-traffic-setup-39d73fcc8b04
 // https://medium.com/swlh/build-an-image-upload-application-with-react-nodejs-postgresql-and-s3-34fe13fbe572
 
+// Singleton:
+// https://refactoring.guru/ru/design-patterns/singleton
+// https://metanit.com/sharp/patterns/2.3.php
+
 class MessageBroker {
-  async messageConsume(photo: any): Promise<void> {
+  async messageConsume(): Promise<void> {
     amqp.connect('amqp://localhost', (connError: any, connection: any) => {
       if (connError) {
         connError;
       }
+
       connection.createChannel((chanError: any, channel: any) => {
         if (chanError) {
           throw chanError;
         }
+
         const queue = 'data_queue';
-        const data = photo;
 
         channel.assertQueue(queue, {
           durable: true,
         });
+
         channel.prefetch(1);
         console.log(
-          ' [*] Waiting for messages in %s. To exit press CTRL+C',
+          '[*] Waiting for messages in %s. To exit press CTRL+C',
           queue
         );
 
         channel.consume(
           queue,
           (data: object[]) => {
-            console.log(' [x] Received %s', data);
+            console.log(' [x] Received ', data);
             setTimeout(() => {
               console.log('[x] Done');
               channel.ack(data);
