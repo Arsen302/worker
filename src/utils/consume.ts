21ef29956b22a6f62d465f5728cfdb70f08213ad
Arsen Arakelyan <aarakelyan98@gmail.com>
2021-03-10 21:34:22 +0200

    change port

--- src/utils/consume.ts
+++ src/utils/consume.ts
@@ -1,5 +1,4 @@
 import * as amqp from 'amqplib';
-import { promisify } from 'util';
 
 const promiseConsume = (ch: amqp.Channel, queue: string) => {
   return new Promise((resolve, reject) => {
@@ -22,8 +21,6 @@ const promiseConsume = (ch: amqp.Channel, queue: string) => {
   });
 };
 
-// consume with async/await syntax
-
 const consume = async (ch: amqp.Channel, queue: string) => {
   try {
     const data: any = await promiseConsume(ch, queue);
@@ -34,48 +31,3 @@ const consume = async (ch: amqp.Channel, queue: string) => {
 };
 
 export default consume;
-
-///////////////////////////////////////////////////////
-
-// // 1. пишем callback функцию consume
-
-// const callbackConsume = (ch: amqp.Channel, queue: string) => {
-//   try {
-//     ch.consume(
-//       queue,
-//       (msg: any) => {
-//         console.log('Received:', JSON.parse(msg.content.toString()));
-//         ch.ack(msg);
-//         console.log('Done!');
-//         return msg;
-//       },
-//       {
-//         noAck: false,
-//       }
-//     );
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
-
-// const data = (msg: any) => {
-//   console.log('Received:', JSON.parse(msg.content.toString()));
-//   ch.ack(msg);
-//   console.log('Done!');
-//   return msg;
-// };
-
-// // 2. преобразовываем callback функию через promisify
-
-// const promiseConsume = promisify(callbackConsume);
-
-// const consume = async (ch: amqp.Channel, queue: string) => {
-//   try {
-//     const data: any = await promiseConsume(ch, queue);
-//     return data;
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
-
-// export default consume;
